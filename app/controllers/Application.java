ecdc6656dcddb68256d69ed8befda92c923d769d
Ben Levy <contact@benlevy.com>
2013-11-28 21:58:59 -0600

    Updated the views so the about section was a separate file

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,12 +8,12 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Smarter Playlists", "home", "Content"));
+        return ok(index.render("Smarter Playlists", "Content"));
 
     }
 
     public static Result about() {
-        return ok(index.render("About", "about", "All about the site"));
+        return ok(about.render("About", "All about the site"));
     }
 
 }
