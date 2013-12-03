5a907d4d6b42643a9e9676fc5475813fd9a01029
Ben Levy <contact@benlevy.com>
2013-12-02 21:24:33 -0600

    Added file upload and some validation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import java.io.File;
+
 import play.*;
 import play.mvc.*;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 
 import views.html.*;
 
@@ -9,8 +13,32 @@ public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Smarter Playlists", "Content"));
-
     }
+
+	@BodyParser.Of(value = BodyParser.MultipartFormData.class, maxLength = 25 * 1024 * 1024)
+	public static Result upload() {
+		MultipartFormData body = request().body().asMultipartFormData();
+		if (request().body().isMaxSizeExceeded()) {
+			flash("error", "File exceeded maximum size");
+			return redirect(routes.Application.index());
+		}
+		FilePart playlist = body.getFile("playlist");
+		if (playlist != null) {
+			String fileName = playlist.getFilename();
+			String contentType = playlist.getContentType();
+			if (!contentType.equals("text/xml")) {
+				flash("error", "Incorrect content type");
+				return redirect(routes.Application.index());
+			}
+			else {
+				File file = playlist.getFile();
+				return ok("Uploaded file: " + fileName);
+			}
+		} else {
+			flash("error", "Missing file");
+			return redirect(routes.Application.index());
+		}
+	}
 
     public static Result about() {
         return ok(about.render("About", "All about the site"));
