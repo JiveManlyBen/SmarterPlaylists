fb1538983c9790e16c236f3d2ecf95c68d1b5f31
Ben Levy <contact@benlevy.com>
2013-12-06 20:28:50 -0600

    Added validation for the iTunes playlist files

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,8 +2,21 @@ package controllers;
 
 import java.io.File;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.XMLConstants;
+
+import org.w3c.dom.Document;
+
 import play.*;
+import play.api.i18n.Lang;
 import play.i18n.Messages;
+import play.Logger;
 import play.mvc.*;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
@@ -33,8 +46,26 @@ public class Application extends Controller {
 			}
 			else {
 				File file = playlist.getFile();
-				return redirect(routes.Application.download());
+				DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+				domFactory.setValidating(true);
+				try {
+					DocumentBuilder builder = domFactory.newDocumentBuilder();
+					Document doc = builder.parse(file);
 
+					Source xmlFile = new StreamSource(file);
+					SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+					Schema schema = schemaFactory.newSchema(new File("conf"+file.separator+"iTunes.xsd"));
+					Validator validator = schema.newValidator();
+					validator.validate(xmlFile);
+
+					return redirect(routes.Application.download());
+				}
+				catch (Exception ex) {
+					flash("error", Messages.get("upload.error.parse"));
+					if (Logger.isDebugEnabled())
+						Logger.debug("File: " + playlist.getFilename(), ex);
+					return redirect(routes.Application.index());
+				}
 			}
 		} else {
 			flash("error", Messages.get("upload.error.missing"));
--- conf/messages.es
+++ conf/messages.es
@@ -7,3 +7,4 @@ upload.button.upload=Subir
 upload.error.contenttype=El archivo subido debe ser un archivo XML biblioteca de iTunes.
 upload.error.maxsize=El archivo subido excede el límite de subida de 25 MB.
 upload.error.missing=El archivo XML de la biblioteca de iTunes no estaba en la subida.
+upload.error.parse=El archivo XML biblioteca de iTunes no se pudo analizar.
