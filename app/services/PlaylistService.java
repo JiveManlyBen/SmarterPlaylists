c34527d3fd1b10017d6900766e10f34c80380f9e
Ben Levy <contact@benlevy.com>
2013-12-10 23:59:35 -0600

    First try at parsing playlist

--- app/services/PlaylistService.java
+++ app/services/PlaylistService.java
@@ -3,6 +3,11 @@ package services;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,8 +19,14 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import org.xml.sax.SAXException;
+
+import domain.PlayList;
+
+import play.Logger;
 
 public class PlaylistService {
 	public static String parseXMLFile(File file) throws SAXException, IOException, ParserConfigurationException {
@@ -29,5 +40,34 @@ public class PlaylistService {
 		DocumentBuilder builder = domFactory.newDocumentBuilder();
 		Document doc = builder.parse(file);
 		return "ok";
+	}
+	private static List<Node> getElementNodes(NodeList nodes) {
+		int nodeCount = nodes.getLength();
+		List<Node> nodeList = new ArrayList<Node>();
+		for (int i = 0; i < nodeCount; i++)
+			if (nodes.item(i).getNodeType() == Node.ELEMENT_NODE)
+				nodeList.add(nodes.item(i));
+		return nodeList;
+	}
+	private static Node getFirstElementNode(NodeList nodeList, String elementName) {
+		for (Node n : getElementNodes(nodeList)) {
+			if (n.getNodeName().equals(elementName))
+				return n;
+		}
+		return null;
+	}
+	private static Map<String, String> getKeysAndValues(NodeList nodeList) {
+		Map<String, String> keyMap = new HashMap<String, String>();
+		String key = null;
+		for (Node node : getElementNodes(nodeList)) {
+			if (node.getNodeName().equals("key")) {
+				key = node.getTextContent();
+			}
+			else if (key != null) {
+				keyMap.put(key, node.getTextContent());
+				key = null;
+			}
+		}
+		return keyMap;
 	}
 }
